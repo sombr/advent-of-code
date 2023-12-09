307237c0c7f0111cac8e32cb93c136bd5c820a63
Serge Toro <sombr@users.noreply.github.com>
2023-12-09 14:56:24 +0000

    day 9: folding this under the carpet

---
+++ 2023/day9/julia-part1.jl
@@ -0,0 +1,47 @@
+#!/usr/bin/env julia
+
+function get_deltas(xs::Vector{Int32})::Vector{Int32}
+    res = []
+    for idx in 2:length(xs)
+        push!(res, xs[idx] - xs[idx-1])
+    end
+
+    return res
+end
+
+function steps_to_zeros(xs::Vector{Int32})::Vector{Int32}
+    last_elements = []
+
+    cur = xs
+    while !all([ x == 0 for x in cur ])
+        push!(last_elements, cur[end])
+        cur = get_deltas(cur)
+    end
+
+    push!(last_elements, 0)
+    return last_elements
+end
+
+function main(filename::String)
+    seqs = [
+        [ parse(Int32, n) for n in split(strip(line), r"\s+") ]
+        for line in readlines(filename)
+    ]
+
+    forecast = [
+        foldr(+, steps_to_zeros(seq))
+        for seq in seqs
+    ]
+
+    println(seqs)
+    println(forecast)
+
+    total = reduce(+, forecast)
+    println("total: $total")
+end
+
+if length(ARGS) < 1
+    println("usage: ", PROGRAM_FILE, " <input>")
+else
+    main(ARGS[1])
+end
