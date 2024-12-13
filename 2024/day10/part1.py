7be5db8b594465b535078856d8edc24308ca7eca
Serge Toro <sombr@users.noreply.github.com>
2024-12-13 12:20:30 +0000

    well, this part2 took embarrassingly long and was super easy in the end

---
+++ 2024/day10/part1.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+
+import sys
+
+def dfs(geo, r, c):
+    trails = 0
+    stack = [ (r, c) ]
+
+    visited = set()
+    while stack:
+        (r, c) = stack.pop()
+
+        if (r, c) in visited:
+            continue
+
+        if geo[r][c] == 9:
+            trails += 1
+
+        visited.add( (r,c) )
+
+        for dr, dc in ( (-1,0), (1,0), (0,-1), (0,1) ):
+            nr, nc = r+dr, c+dc
+            if nr < 0 or nc < 0 or nr >= len(geo) or nc >= len(geo[0]):
+                continue
+            if (nr, nc) in visited:
+                continue
+            if geo[nr][nc] - geo[r][c] != 1:
+                continue
+
+            stack.append( (nr, nc) )
+
+    return trails
+
+def main():
+    geo = []
+    with open(sys.argv[1], "r") as file:
+        for line in file:
+            geo.append([ int(x) for x in line.strip() ])
+
+    trailheads = []
+    for r in range(len(geo)):
+        for c in range(len(geo[r])):
+            if geo[r][c] == 0:
+                trails  = dfs(geo, r, c)
+                trailheads.append( trails )
+
+    part1 = sum(trailheads)
+    print(f"part1: {part1}")
+
+main()
