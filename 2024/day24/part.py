bf3fb284db17932e3fdcfe05000f0796daa47e43
Serge Toro <sombr@users.noreply.github.com>
2024-12-24 22:12:17 +0000

    painful heuristics search

--- 2024/day24/part.py
+++ 2024/day24/part.py
@@ -24,20 +24,48 @@ def load_input(filename):
 
     return wires, links
 
-def compute_values(start, state, links):
+def print_path(links, state, start, level):
+    indent = level * 2
+    if start in links:
+        print(" " * indent, end = "")
+        op, a, b = links[start]
+        desc = str(level)
+
+        if op == "XOR":
+            if a.startswith("x") or a.startswith("y"):
+                desc = f"SUM"
+            else:
+                desc = f"SUM+CARRY"
+        elif op == "OR":
+            desc = f"CARRY"
+        elif op == "AND":
+            if a.startswith("x") or b.startswith("y"):
+                desc = f"BITCARRY"
+            else:
+                desc = f"CARRY"
+
+        val = state[start]
+        print(f"{start} ({desc}) [{val}] <= {a} {op} {b}")
+        print_path(links, state, a, level+1)
+        print_path(links, state, b, level+1)
+
+def compute_values(start, state, links, level=0):
+    if level > 100:
+        return -1000
+
     if start in state:
         return state[start]
 
+    value = -1000
     if start in links:
         op, a, b = links[start]
-        value = None
         match op:
             case "AND":
-                value = compute_values(a, state, links) * compute_values(b, state, links)
+                value = compute_values(a, state, links, level+1) * compute_values(b, state, links, level+1)
             case "OR":
-                value = min(compute_values(a, state, links) + compute_values(b, state, links), 1)
+                value = min(compute_values(a, state, links, level+1) + compute_values(b, state, links, level+1), 1)
             case "XOR":
-                value = (compute_values(a, state, links) + compute_values(b, state, links)) % 2
+                value = (compute_values(a, state, links, level+1) + compute_values(b, state, links, level+1)) % 2
 
         state[start] = value
 
@@ -58,7 +86,18 @@ def compute_output(wires, links):
             pos = int(wire[1:])
             total = total | (value << pos)
 
-    return total
+    return total, state
+
+def check_correct(x, y, shape, links):
+    wires = {}
+    num2wires(x, "x", shape, wires)
+    num2wires(y, "y", shape, wires)
+
+    value, state = compute_output(wires, links)
+
+    true_answer = x+y
+
+    return value == true_answer
 
 def main():
     wires, links = load_input(sys.argv[1])
@@ -83,24 +122,56 @@ def main():
 
     print(shape_x, shape_y, shape_z)
 
-    # fuzzing
-    incorrect_outs = {}
-    for x in tqdm(range(0, (1<<shape_x)-1, 131123182323)):
-        for y in range(0, (1<<shape_y)-1, 121291271233):
-            wires = {}
-            num2wires(x, "x", shape_x, wires)
-            num2wires(y, "y", shape_y, wires)
+    wrong_offs = []
+    for off in range(44):
+        x = 1<<off
+        y = 1<<off
+        if not check_correct(x, y, shape_x, links):
+            wrong_offs.append(off)
 
-            value = compute_output(wires, links)
+    print(wrong_offs)
 
-            true_answer = x+y
+    wrong_offs = []
+    for off in range(44):
+        x = 1<<off
+        y = 1<<off+1
+        if not check_correct(x, y, shape_x, links):
+            wrong_offs.append(off)
 
-            for idx in range(shape_z):
-                ta = ((true_answer >> idx)&1)
-                va = ((value >> idx)&1)
-                name = "z%02d" % idx
-                if ta != va:
-                    incorrect_outs[name] = incorrect_outs.get(name, 0) + 1
-    print(incorrect_outs)
+    print(wrong_offs)
+
+    wrong_offs = []
+    for x in range(1<<43):
+        y = 1
+        if not check_correct(x, y, shape_x, links):
+            wrong_offs.append(off)
+            break
+
+    print(wrong_offs)
+    
+    exit(1)
+
+    gates = sorted(links.keys())
+    for agi in range(len(gates)):
+        ag = gates[agi]
+        for bgi in range(agi+1, len(gates)):
+            bg = gates[bgi]
+            links[ag], links[bg] = links[bg], links[ag]
+
+            all_correct = True
+            for off in range(44):
+                x = 1<<off
+                y = 1<<off
+                if not check_correct(x, y, shape_x, links):
+                    all_correct = False
+                y = 1<<off+1
+                if not check_correct(x, y, shape_x, links):
+                    all_correct = False
+            if all_correct:
+                print(f"found candidate for swap: {ag} <=> {bg}")
+
+            links[ag], links[bg] = links[bg], links[ag]
+
+    print(wrong_offs)
 
 main()
